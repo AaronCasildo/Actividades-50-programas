0785213c7aab04aa8ad123963f1da3e194d2516b
AaronCasildo <l22212222@tectijuana.edu.mx>
2024-11-10 16:49:51 -0800

    Update Programa14BusLineal.s

--- Programa14BusLineal.s
+++ Programa14BusLineal.s
@@ -1,106 +1,117 @@
-// Programa: busqueda_lineal.s
-// Autor: Aaron Casildo Rubalcava
-// Descripción: Realiza una búsqueda lineal de un número en un arreglo
-// Entrada: Arreglo predefinido en .data y número a buscar
-// Salida: Posición del número si se encuentra, o mensaje si no está
-//
-// Implementación equivalente en C++:
-// ```cpp
-// #include <iostream>
-// using namespace std;
-// int main() {
-//     int arr[] = {15, 7, 23, 9, 12, 3, 18, 45, 6, 11};
-//     int size = 10;
-//     int buscar = 18;
-//     bool encontrado = false;
-//     int posicion = -1;
-//     
-//     cout << "Arreglo: ";
-//     for(int i = 0; i < size; i++) {
-//         cout << arr[i] << " ";
-//     }
-//     cout << "\nBuscando: " << buscar << endl;
-//     
-//     for(int i = 0; i < size; i++) {
-//         if(arr[i] == buscar) {
-//             encontrado = true;
-//             posicion = i;
-//             break;
-//         }
-//     }
-//     
-//     if(encontrado) {
-//         cout << "Número encontrado en la posición: " << posicion << endl;
-//     } else {
-//         cout << "Número no encontrado" << endl;
-//     }
-//     return 0;
-// }
-// ```
+/***********************************************************************
+* Programa: Buscar un Número en un Arreglo en ARM64 Assembly
+* Autor: Aaron Casildo Rubalcava
+* Descripción: Este programa busca un número específico en un arreglo de
+*              enteros, imprime el arreglo, luego busca el número
+*              especificado y muestra un mensaje indicando si fue encontrado
+*              o no, junto con su posición en el arreglo.
+* 
+* Compilación:
+*    as -o buscar_num.o buscar_num.s
+*    gcc -o buscar_num buscar_num.o -no-pie
+*
+* Ejecución:
+*    ./buscar_num
+*
+* Traducción a C (para referencia):
+* ----------------------------------------------------
+* #include <stdio.h>
+* 
+* int main() {
+*     long arreglo[] = {15, 7, 23, 9, 12, 3, 18, 45, 6, 11};
+*     int longitud = 10;
+*     long buscar = 18;
+*     int encontrado = -1;
+*     
+*     printf("Arreglo: ");
+*     for (int i = 0; i < longitud; i++) {
+*         printf("%ld ", arreglo[i]);
+*     }
+*     
+*     printf("\nBuscando el número: %ld\n", buscar);
+*     for (int i = 0; i < longitud; i++) {
+*         if (arreglo[i] == buscar) {
+*             printf("Número encontrado en la posición: %d\n", i);
+*             encontrado = 1;
+*             break;
+*         }
+*     }
+*     
+*     if (encontrado == -1) {
+*         printf("Número no encontrado en el arreglo\n");
+*     }
+*     
+*     return 0;
+* }
+*
+* Link de grabación asciinema:
+* https://asciinema.org/a/DTPLjSM4HqD593KSXpzmnCg5P
+***********************************************************************/
 
 .data
-    arreglo:    .quad   15, 7, 23, 9, 12, 3, 18, 45, 6, 11  // Arreglo de números
-    longitud:   .quad   10                                   // Longitud del arreglo
-    buscar:     .quad   18                                   // Número a buscar
-    msg_arr:    .asciz  "Arreglo: "
-    msg_num:    .asciz  "%ld "                              // Formato para imprimir números
-    msg_buscar: .asciz  "\nBuscando el número: %ld\n"       // Mensaje para número a buscar
-    msg_enc:    .asciz  "Número encontrado en la posición: %ld\n" // Mensaje cuando se encuentra
-    msg_no_enc: .asciz  "Número no encontrado en el arreglo\n"    // Mensaje cuando no se encuentra
-    newline:    .asciz  "\n"
+arreglo:    .quad   15, 7, 23, 9, 12, 3, 18, 45, 6, 11  // Arreglo de números
+longitud:   .quad   10                                   // Longitud del arreglo
+buscar:     .quad   18                                   // Número a buscar
+msg_arr:    .asciz  "Arreglo: "
+msg_num:    .asciz  "%ld "                              // Formato para imprimir números
+msg_buscar: .asciz  "\nBuscando el número: %ld\n"       // Mensaje para número a buscar
+msg_enc:    .asciz  "Número encontrado en la posición: %ld\n" // Mensaje cuando se encuentra
+msg_no_enc: .asciz  "Número no encontrado en el arreglo\n"    // Mensaje cuando no se encuentra
+newline:    .asciz  "\n"
 
 .text
 .global main
+
 main:
     // Prólogo
-    stp     x29, x30, [sp, -48]!
-    mov     x29, sp
+    stp     x29, x30, [sp, -48]!   // Guarda el frame pointer y link register
+    mov     x29, sp                 // Establece el frame pointer
 
     // Imprimir mensaje inicial
     ldr     x0, =msg_arr
     bl      printf
 
     // Inicializar variables
-    ldr     x19, =arreglo         // x19 = dirección base del arreglo
-    ldr     x20, =longitud        // Cargar dirección de longitud
-    ldr     x20, [x20]            // x20 = longitud del arreglo
-    mov     x21, #0               // x21 = contador (i)
+    ldr     x19, =arreglo           // x19 = dirección base del arreglo
+    ldr     x20, =longitud          // Cargar dirección de longitud
+    ldr     x20, [x20]              // x20 = longitud del arreglo
+    mov     x21, #0                 // x21 = contador (i)
     
     // Imprimir todos los números del arreglo
 print_loop:
     cmp     x21, x20
-    bge     print_done            // Si i >= longitud, terminar impresión
+    bge     print_done              // Si i >= longitud, terminar impresión
     
     // Imprimir número actual
     ldr     x0, =msg_num
-    ldr     x1, [x19, x21, lsl #3]  // Cargar arreglo[i]
+    ldr     x1, [x19, x21, lsl #3] // Cargar arreglo[i]
     bl      printf
     
-    add     x21, x21, #1         // i++
+    add     x21, x21, #1           // i++
     b       print_loop
     
 print_done:
     // Imprimir el número que vamos a buscar
     ldr     x0, =msg_buscar
     ldr     x1, =buscar
-    ldr     x1, [x1]
+    ldr     x1, [x1]               // Cargar número a buscar
     bl      printf
 
     // Inicializar búsqueda
-    mov     x21, #0              // x21 = contador = 0
-    ldr     x22, =buscar         // Cargar dirección del número a buscar
-    ldr     x22, [x22]           // x22 = número a buscar
+    mov     x21, #0                // x21 = contador = 0
+    ldr     x22, =buscar           // Cargar dirección del número a buscar
+    ldr     x22, [x22]             // x22 = número a buscar
 
 search_loop:
     cmp     x21, x20
-    bge     not_found            // Si i >= longitud, número no encontrado
+    bge     not_found              // Si i >= longitud, número no encontrado
     
     // Cargar y comparar elemento actual
     ldr     x23, [x19, x21, lsl #3]  // x23 = arreglo[i]
     cmp     x23, x22
-    beq     found                // Si son iguales, número encontrado
+    beq     found                  // Si son iguales, número encontrado
     
-    add     x21, x21, #1         // i++
+    add     x21, x21, #1           // i++
     b       search_loop
 
 found:
@@ -118,5 +129,5 @@ not_found:
 done:
     // Epílogo y retorno
     mov     w0, #0
-    ldp     x29, x30, [sp], 48
-    ret
+    ldp     x29, x30, [sp], 48     // Restaura el frame pointer y link register
+    ret                            // Retorna al sistema operativo
